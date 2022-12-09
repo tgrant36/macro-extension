ba07fd5932d42bc2583c474b7b8d412b130dcfce
Tyler Grant <tjgrant36@gmail.com>
2022-12-08 22:26:34 -0500

    Re-organized code base. Added shell for record macro

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,13 @@
 const playMacroId = document.getElementById("playMacroId");
+const recordMacroId = document.getElementById("recordMacroId");
+
 if (playMacroId) {
     playMacroId.onclick = function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             chrome.tabs.sendMessage(
                 tabs[0].id,
                 {
-                    url: chrome.runtime.getURL("images/image.png"),
+                    url: chrome.runtime.getURL("images/web-image.png"),
                     imageDivId: `${guidGenerator()}`,
                     tabId: tabs[0].id
                 },
@@ -13,12 +15,19 @@ if (playMacroId) {
                     window.close();
                 }
             );
-            function guidGenerator() {
-                const S4 = function () {
-                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                };
-                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-            }
         });
   };
 }
+
+if(recordMacroId) {
+    recordMacroId.onclick = function() {
+        console.log("Will start recording! Once I build it....")
+    }
+}
+
+function guidGenerator() {
+    const S4 = function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+};
